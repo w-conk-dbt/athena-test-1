30c0eabe63bbdbf3bf57496b3e5e867cfb8b0057
Will Conklin <will.conklin@fishtownanalytics.com>
2025-05-14 15:02:14 +0000

    added column

--- models/staging/stg_tpch_nations.sql
+++ models/staging/stg_tpch_nations.sql
@@ -11,7 +11,8 @@ renamed as (
         n_nationkey as nation_key,
         n_name as name,
         n_regionkey as region_key,
-        n_comment as comment
+        n_comment as comment,
+        1 as test_column
 
     from source
 
